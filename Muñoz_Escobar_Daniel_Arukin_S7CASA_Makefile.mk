3677fd3be45ef69bf33e9e22ffc59cd6ff68a767
DanielArukin <162343794+DanielArukin@users.noreply.github.com>
2024-03-29 11:33:31 -0500

    Update Muñoz_Escobar_Daniel_Arukin_S7CASA_Makefile.mk

--- Muñoz_Escobar_Daniel_Arukin_S7CASA_Makefile.mk
+++ Muñoz_Escobar_Daniel_Arukin_S7CASA_Makefile.mk
@@ -1,12 +1,28 @@
 resultados.pdf : muñoz_escobar_daniel_arukin_s7casa_plots.pdf
 
-muñoz_escobar_daniel_arukin_s7casa_plots.pdf : muñoz_escobar_daniel_arukin_s7casa_plots.py sol_fijo.dat sol_libre.dat
+muñoz_escobar_daniel_arukin_s7casa_plots.pdf : muñoz_escobar_daniel_arukin_s7casa_plots.py datos_euler_2orden_sol_fijo.dat datos_LeapFrog_sol_fijo.dat datos_tierra_euler_2orden_sol_libre.dat datos_sol_libre_euler_2orden.dat datos_tierra_LeapFrog_sol_libre.dat datos_sol_libre_LeapFrog.dat
 	python3 muñoz_escobar_daniel_arukin_s7casa_plots.py
 
-sol_fijo.dat : Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolFijo.cpp
+datos_euler_2orden_sol_fijo.dat : Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolFijo.cpp
 	g++ Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolFijo.cpp
 	./a.out>>sol_fijo.dat
 
-sol_libre.dat : Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolLibre.cpp
+datos_LeapFrog_sol_fijo.dat : Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolFijo.cpp
+	g++ Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolFijo.cpp
+	./a.out>>datos_LeapFrog_sol_fijo.dat
+
+datos_tierra_euler_2orden_sol_libre.dat : Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolLibre.cpp
 	g++ Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolLibre.cpp
-	./a.out>>sol_libre.dat
+	./a.out>>datos_tierra_euler_2orden_sol_libre.dat
+
+datos_sol_libre_euler_2orden.dat : Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolLibre.cpp
+	g++ Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolLibre.cpp
+	./a.out>>datos_sol_libre_euler_2orden.dat
+
+datos_tierra_LeapFrog_sol_libre.dat : Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolLibre.cpp
+	g++ Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolLibre.cpp
+	./a.out>>datos_tierra_LeapFrog_sol_libre.dat
+
+datos_sol_libre_LeapFrog.dat : Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolLibre.cpp
+	g++ Muñoz_Escobar_Daniel_Arukin_S7CASA_TierraSolLibre.cpp
+	./a.out>>datos_sol_libre_LeapFrog.dat
