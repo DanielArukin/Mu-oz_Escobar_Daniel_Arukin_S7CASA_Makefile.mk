48fc4587614461ec3f00e6c900f69d68044144c6
DanielArukin <162343794+DanielArukin@users.noreply.github.com>
2024-03-29 12:03:21 -0500

    Update Muñoz_Escobar_Daniel_Arukin_S7CASA_Makefile.mk

--- Muñoz_Escobar_Daniel_Arukin_S7CASA_Makefile.mk
+++ Muñoz_Escobar_Daniel_Arukin_S7CASA_Makefile.mk
@@ -1,4 +1,4 @@
-resultados.pdf : muñoz_escobar_daniel_arukin_s7casa_plots.pdf
+resultados.pdf : resultados.tex muñoz_escobar_daniel_arukin_s7casa_plots.pdf
 
 muñoz_escobar_daniel_arukin_s7casa_plots.pdf : muñoz_escobar_daniel_arukin_s7casa_plots.py datos_euler_2orden_sol_fijo.dat datos_LeapFrog_sol_fijo.dat datos_tierra_euler_2orden_sol_libre.dat datos_sol_libre_euler_2orden.dat datos_tierra_LeapFrog_sol_libre.dat datos_sol_libre_LeapFrog.dat
 	python3 muñoz_escobar_daniel_arukin_s7casa_plots.py
